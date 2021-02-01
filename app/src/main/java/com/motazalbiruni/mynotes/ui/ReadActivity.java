53d88c0c8a97e7fab56d56f4f9ab3ef42d299943
motaz albiruni <motaz.albiruni@gmail.com>
2021-02-01 23:03:26 +0200

    My Notes edition 5

--- app/src/main/java/com/motazalbiruni/mynotes/ui/ReadActivity.java
+++ app/src/main/java/com/motazalbiruni/mynotes/ui/ReadActivity.java
@@ -8,10 +8,13 @@ import androidx.lifecycle.ViewModelProvider;
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -30,13 +33,14 @@ public class ReadActivity extends AppCompatActivity {
     private MainViewModel viewModel;
     private EditText txt_title, txt_body;
     private Spinner spinner;
+    LinearLayout linearLayout;
     private static Integer id_note;
     public static final String KEY_ID = "id_key";
 
     private ArrayAdapter<String> adapterSpinner;
     private List<String> list_spinner;
     private static int type=0;
-
+    int x = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +53,7 @@ public class ReadActivity extends AppCompatActivity {
         txt_title = findViewById(R.id.txtTitle_Read); //text for title note
         txt_body = findViewById(R.id.txtNote_Read); //text for body note
         spinner = findViewById(R.id.spinner_important);//choose the type of important to note
+        linearLayout = findViewById(R.id.linearLayout_body);
         //setting text size in reading
         String textSize = pref.getString("textSize", "welcome");
         switch (textSize){
@@ -70,7 +75,6 @@ public class ReadActivity extends AppCompatActivity {
         list_spinner = Arrays.asList(getResources().getStringArray(R.array.typeNotes));
 
         //adapter of spinner
-//        adapterSpinner = new ArrayAdapter<>(this,R.layout.spinner_item_blue,list_spinner);
         adapterSpinner = new ArrayAdapter<>(this, R.layout.spinner_item_blue
                 , R.id.text_spinner_item_blue, list_spinner);
         spinner.setAdapter(adapterSpinner);
@@ -105,7 +109,8 @@ public class ReadActivity extends AppCompatActivity {
             editData();
         }
 
-    }
+    }//end onCreate()
+
     //create option menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -132,6 +137,7 @@ public class ReadActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }//end onOptionsItemSelected()
+
 
     private void saveData(){
         Toast.makeText(this, getResources().getString(R.string.save), Toast.LENGTH_SHORT).show();
